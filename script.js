64879184278af888b9dd5e498eecc55ab77e215c
mlorber <87738509+mlorberdev@users.noreply.github.com>
2022-12-05 09:58:21 -0500

    timer update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     const points = [1, 4, 4, 2, 1, 4, 3, 3, 1, 10, 5, 2, 4, 2, 1, 4, 10, 1, 1, 1, 2, 5, 4, 8, 3, 10];
     const connected = [[4, 5, 1], [0, 4, 5, 6, 2], [1, 3, 5, 6, 7], [2, 6, 7], [0, 1, 5, 8, 9], [0, 1, 2, 4, 6, 8, 9, 10], [1, 2, 3, 5, 7, 9, 10, 11], [2, 3, 6, 10, 11], [4, 5, 9, 12, 13], [4, 5, 6, 8, 10, 12, 13, 14], [5, 6, 7, 9, 11, 13, 14, 15], [6, 7, 10, 14, 15], [8, 9, 13], [8, 9, 10, 12, 14], [9, 10, 11, 13, 15], [10, 11, 14]];
     let pp = [], tempWord = [], wordList = [], score = 0, inputs = [], board, ttt = document.getElementById("tempWord");
-    let timing = false, time = 12;
+    let timing = false, time = 180;
 
     function timer() {
         if (timing === false) {return}
